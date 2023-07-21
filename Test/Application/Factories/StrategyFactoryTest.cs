af6b351902e19a7b99f333097fc685cd93196740
Diego Murillo <Diego.Murillo@jalasoft.com>
2023-07-21 09:54:41 -0500

    Create unit test for controllers and strategy factory

---
+++ Test/Application/Factories/StrategyFactoryTest.cs
@@ -0,0 +1,52 @@
+﻿using InsuranceAPIv2.Application.Contracts;
+using InsuranceAPIv2.Application.Factories;
+using InsuranceAPIv2.Shared.Helpers;
+using Xunit;
+
+namespace InsuranceAPIv2.Test.Application.Factories
+{
+    public class StrategyFactoryTest
+    {
+        [Fact]
+        public void GetStrategy_ThrowsException_OnUnsupportedCarrier()
+        {
+            IEnumerable<IDummyStrategy> strategies = new List<IDummyStrategy> { new DummyStrategy() };
+
+            StrategyFactory<IDummyStrategy> strategyFactory = new(strategies);
+
+            Assert.Throws<NotSupportedException>(() => strategyFactory.GetStrategy(Carrier.Vsp));
+        }
+
+        [Fact]
+        public void GetStrategy_ThrowsException_OnEmptyStrategyList()
+        {
+            IEnumerable<IDummyStrategy> strategies = new List<IDummyStrategy>() { };
+
+            StrategyFactory<IDummyStrategy> strategyFactory = new(strategies);
+
+            Assert.Throws<NotSupportedException>(() => strategyFactory.GetStrategy(Carrier.Vsp));
+        }
+
+        [Fact]
+        public void GetStrategy_ReturnValidStrategy_OnSupportedCarrier()
+        {
+            IEnumerable<IDummyStrategy> strategies = new List<IDummyStrategy> { new DummyStrategy() };
+
+            StrategyFactory<IDummyStrategy> strategyFactory = new(strategies);
+
+            IDummyStrategy strategy = strategyFactory.GetStrategy(Carrier.EyeMed);
+
+            Assert.Equal(Carrier.EyeMed, strategy.SupportedCarrier);
+        }
+    }
+
+    public interface IDummyStrategy: IStrategy
+    {
+
+    }
+
+    public class DummyStrategy : IDummyStrategy
+    {
+        public Carrier SupportedCarrier => Carrier.EyeMed;
+    }
+}
---
+++ Test/Presentation/Controllers/BenefitsControllerTest.cs
@@ -0,0 +1,45 @@
+﻿using InsuranceAPIv2.Application.Contracts;
+using InsuranceAPIv2.Application.DTOs;
+using InsuranceAPIv2.Presentation.Controllers;
+using InsuranceAPIv2.Shared.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace InsuranceAPIv2.Test.Presentation.Controllers
+{
+    public class BenefitsControllerTest
+    {
+        [Fact]
+        public async Task GetPatientBenefits_ReturnsBadRequest_OnInvalidResult()
+        {
+            Mock<IBenefitContext> benefitContext = new();
+
+            Error error = new() { Code = Code.BadRequest, Message = "Invalid Carrier Id" };
+
+            benefitContext.Setup(context => context.RetrievePatientBenefits(4,1).Result)
+                .Returns(new Result<IEnumerable<DtoBenefit>> { Error = error});
+
+            BenefitsController benefitsController = new(benefitContext.Object);
+
+            IActionResult result = await benefitsController.GetPatientBenefits(4, 1);
+
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
+
+        [Fact]
+        public async Task GetPatientBenefits_ReturnsOk_OnValidResult()
+        {
+            Mock<IBenefitContext> benefitContext = new();
+
+            benefitContext.Setup(context => context.RetrievePatientBenefits(1,1).Result)
+                .Returns(new Result<IEnumerable<DtoBenefit>> { Data = new List<DtoBenefit>() });
+
+            BenefitsController benefitsController = new(benefitContext.Object);
+
+            IActionResult result = await benefitsController.GetPatientBenefits(1, 1);
+
+            Assert.IsType<OkObjectResult>(result);
+        }
+    }
+}
